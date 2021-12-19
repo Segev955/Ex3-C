8498333c5253cb2647664a878ef3d0ccf85841cf
Segev955 <Segev95@gmail.comgit config --global user.email Segev95@gmail.com>
2021-12-19 16:41:18 +0200

    add atbash

--- main.h
+++ main.h
@@ -7,5 +7,9 @@ void geo(int fsum, char text[],char s[]);
 void anagram(char text[],char s[],char word[]);
 void removeChar(char tempWord[],char s1);
 int contain(char tempWord[]);
+void toAtbash(char atWord[]);
+void reverse(char reWord[],char atWord[]);
+int checkWord(char atWord[],char tempWord[]);
+void atbash(char atWord[],char reWord[],char text[], char s[]);
 
 #endif
