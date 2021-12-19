fa94189a40b049038c34468cb538a99a49158c37
Segev955 <Segev95@gmail.comgit config --global user.email Segev95@gmail.com>
2021-12-19 16:41:15 +0200

    add atbash

--- main.c
+++ main.c
@@ -21,18 +21,27 @@ int main() {
         }
         scanf("%c", &text[i]);
     }
-
+    //Gematria
     int sum = sumOfWord(word);
     char s1[TXT];
     geo(sum,text,s1);
     printf("\n");
     printf("Gematria Sequences: ");
     printf("%s", s1);
-/*    char s2[TXT];
-    atbash(text,s2,word);
+    
+    //Atbash
+    char atWord[WORD];
+    strcpy(atWord, word);
+    toAtbash(atWord);
+    char reWord[strlen(atWord)];
+    reverse(reWord,atWord);
+    char s2[TXT];
+    atbash(atWord,reWord,text,s2);
     printf("\n");
     printf("Atbash Sequences: ");
-    printf("%s", s2);*/
+    printf("%s", s2);
+    //Anagram
+    
     char s3[TXT];
     anagram(text,s3,word);
     printf("\n");
@@ -98,6 +107,91 @@ void geo(int fsum, char text[], char s[]) {
     *(s + y - 1) = '\0';
 }
 
+void toAtbash(char atWord[]) {
+    for (int i = 0; i < strlen(atWord); i++) {
+        if (atWord[i] >= 'A' && atWord[i] <= 'Z') {
+            *(atWord+i) = 'Z' - (atWord[i] - 'A');
+
+        } else if (atWord[i] >= 'a' && atWord[i] <= 'z') {
+            *(atWord+i) = 'z' - (atWord[i] - 'a');
+        } else {
+            continue;
+        }
+    }
+}
+
+void reverse(char reWord[],char atWord[]) {
+    for (int i = 0, j = strlen(atWord)-2; i < strlen(atWord); i++,j--) {
+        *(reWord+i) = atWord[j];
+        *(reWord+i+1) = '\0';
+    }
+}
+
+void atbash(char atWord[],char reWord[],char text[], char s[]) {
+    int x = 0;
+    int y = 0;
+    for (int i = 0; i < strlen(text); i++) {
+        char tempWord[WORD];
+        int t = 0;
+        int t2 = 0;
+        // strcpy(tempWord, atWord);
+        if ((text[i] >= 'A' && text[i] <= 'Z') || (text[i] >= 'a' && text[i] <= 'z')) {
+        for (int j = i; j < strlen(text); j++) {
+                char s1 = text[j];
+                if (s1 == ' ') {
+                    *(s + x) = s1;
+                    tempWord[t] = s1;
+                    t += 1;
+                    tempWord[t] = '\0';
+                    x += 1;
+                    continue;
+                }
+                if (strchr(atWord, s1)) { // if word contain char
+                    *(s + x) = s1;
+                    tempWord[t] = s1;
+                    t += 1;
+                    t2 += 1;
+                    tempWord[t] = '\0';
+                    x += 1;
+                }
+                else {
+                    *(s + y) = '\0';
+                    x = y;
+                    break;
+                }
+                if (t2 == strlen(atWord)-1) { // 
+                    if (checkWord(atWord,tempWord) || checkWord(reWord,tempWord)) {
+                        *(s + x) = '~';
+                        y = x + 1;
+                        x += 1;
+                        break;
+                    }
+                }
+            }
+        }
+    }
+    *(s + y - 1) = '\0';
+}
+
+// checks if the tempWord equal to atWord or reWord
+int checkWord(char atWord[],char tempWord[]) {
+    if (tempWord[0] == atWord[0]) {
+        for (int i = 0, j = 0; i < strlen(tempWord); i++) {
+            if (tempWord[i] == ' ') {
+                continue;
+            }
+            if (tempWord[i] != atWord[j]) {
+                return 0;
+            }
+            j++;
+        }
+    }
+    else {
+        return 0;
+    }
+    return 1;
+}
+
 void anagram(char text[], char s[], char word[]) {
     int x = 0;
     int y = 0;
@@ -119,13 +213,13 @@ void anagram(char text[], char s[], char word[]) {
                     x += 1;
                     removeChar(tempWord, s1);
                     
-                } else {
+                } 
+                else {
                     *(s + y) = '\0';
                     x = y;
                     break;
                 }
                 if (contain(tempWord) == 1) { // if tempWord dont contain chars from word
-
                     *(s + x) = '~';
                     y = x + 1;
                     x += 1;
@@ -155,3 +249,5 @@ int contain(char tempWord[]) {
     }
     return 1;
 }
+
+
